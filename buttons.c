fd751a31dc101b8ab8184ba0ee3a1b092706e88f
MMacPhee <malcolm.n.macphee@gmail.com>
2018-03-15 01:46:08 -0700

    still plugging away
    ay ayyyy

--- buttons.c
+++ buttons.c
@@ -7,49 +7,49 @@ void poll_buttons() {
   short buttons[5];
 
   buttons[0] = digitalRead(view_btn);
-    Serial.print("Button 1 is: ");
+    /*Serial.print("Button 1 is: ");
     Serial.println(buttons[0]);
     lcd.setCursor(11, 1);
     if (buttons[0] == HIGH)
       lcd.print(1);
     else
-      lcd.print(0);
+      lcd.print(0);*/
       
     buttons[1] = digitalRead(edit_btn);
-    lcd.setCursor(12, 1);
+    /*lcd.setCursor(12, 1);
     Serial.print("Button 2 is: ");
     Serial.println(buttons[1]);
     if (buttons[1] == HIGH)
       lcd.print(1);
     else
-      lcd.print(0); 
+      lcd.print(0); */
        
     buttons[2] = digitalRead(up_btn);
-    Serial.print("Button 3 is: ");
+    /*Serial.print("Button 3 is: ");
     Serial.println(buttons[2]);
     lcd.setCursor(13, 1);
     if (buttons[2] == HIGH)
       lcd.print(1);
     else
-      lcd.print(0);
+      lcd.print(0);*/
       
     buttons[3] = digitalRead(down_btn);
-    Serial.print("Button 4 is: ");
+    /*Serial.print("Button 4 is: ");
     Serial.println(buttons[3]);
     lcd.setCursor(14, 1);
     if (buttons[3] == HIGH)
       lcd.print(1);
     else
-      lcd.print(0);
+      lcd.print(0);*/
       
     buttons[4] = digitalRead(start_btn);
-    Serial.print("Button 5 is: ");
+    /*Serial.print("Button 5 is: ");
     Serial.println(buttons[4]);
     lcd.setCursor(15, 1);
     if (buttons[4] == HIGH)
       lcd.print(1);
     else
-      lcd.print(0);
+      lcd.print(0);*/
 
     void write_btn("view", buttons[0]);
     void write_btn("edit", buttons[1]);
